430ae129da2c14fe14bc3a862c4f40b89f637174
[ユ mochimaru <akimochi18@gmail.com>
2018-01-20 12:18:10 +0900

    commit

--- app/controllers/news_watch_controller.rb
+++ app/controllers/news_watch_controller.rb
@@ -7,7 +7,9 @@ class NewsWatchController < ApplicationController
     #yahoo
     url = "https://news.yahoo.co.jp/pickup/rss.xml"
     rss = RSS::Parser.parse(url)
+    #x.pubDate.today.strftime("%Y年 %m月 %d日") 
     @yahooData = rss.channel.items
+    #p @yahooData
     
     #GIGAZINE
     url = "http://feed.rssad.jp/rss/gigazine/rss_2.0"
@@ -18,5 +20,11 @@ class NewsWatchController < ApplicationController
     url = "http://feeds.rocketnews24.com/rocketnews24"
     rss = RSS::Parser.parse(url)
     @rocketData = rss.channel.items
+    
+    #天気
+    uri = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=140020')
+    json = Net::HTTP.get(uri)
+    @weatherData = JSON.parse(json)
+    p @weatherData["pinpointLocations"]
   end  
 end
